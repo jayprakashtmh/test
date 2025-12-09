97c1cfff79a30b066e955f8bf89c73940c8a0884
jayprakashtmh <jayprakashtmh@gmail.com>
2025-12-09 16:10:33 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,39 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+
+public class Controller {
+  @PostMapping
+  public String testPostMapping(@RequestBody String s)
+  {
+	  System.out.println(s);
+	  return s;
+  }
+  @GetMapping
+  public String testgetMapping(@RequestParam String s )
+  {
+	  System.out.println(s);
+	  return s;
+  }
+  @PutMapping
+  public String putMapping(@PathVariable String s)
+  {
+	  System.out.println(s);
+	  return s;
+  }
+@org.springframework.web.bind.annotation.DeleteMapping
+  public String DeleteMapping(@PathVariable String s) {
+	System.out.println(s);
+	  return s;
+  }
+}
